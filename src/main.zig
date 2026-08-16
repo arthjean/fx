f8a3912942922e75f2bc3872acb776faaf60e523
Pranit <pranit@vercel.com>
2026-08-15 20:09:05 -0400

    Use JS-host stream in WASM adapter

--- src/main.zig
+++ src/main.zig
@@ -442,8 +442,10 @@ const App = struct {
         try self.flushRequestedFrame();
     }
 
-    pub fn providerAdapter(_: *const Self) agent_stream_provider.ProviderAdapter {
-        return builtin_gateway.provider_adapter;
+    pub fn providerAdapter(self: *const Self) agent_stream_provider.ProviderAdapter {
+        var adapter = builtin_gateway.provider_adapter;
+        adapter.legacy_provider = self.agentStreamProvider();
+        return adapter;
     }
 
     pub fn secretStore(self: *const Self) host.SecretStore {
